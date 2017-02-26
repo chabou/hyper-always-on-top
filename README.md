81c19f6dbf6aafd4daf198322163d36fdadd5dc3
CHaBou <chabup@delean.fr>
2017-02-26 19:39:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# hyper-always-on-top
+Extension for [Hyper.app](https://hyper.is) to add a menu item to keep application windows always on top.
+
+![Screenshot](https://cloud.githubusercontent.com/assets/4137761/23342457/ed609bea-fc5a-11e6-94a4-9fea87470dcb.png)
+
+## Install
+
+### Using [hpm](https://github.com/zeit/hpm)
+
+```
+hpm install hyper-always-on-top
+```
+
+### Manually
+
+To install, edit `~/.hyper.js` and add `"hyper-always-on-top"` to `plugins`:
+
+```
+plugins: [
+  "hyper-always-on-top",
+],
+```
+
+## Configuration
+
+### Default configuration:
+``` js
+module.exports = {
+  config: {
+    // other configs...
+    alwaysOnTop: {
+      debug: false,
+      default: yes // enabled on application start
+    }
+  }
+  //...
+};
+```
+
+## Credits
+
+Originally developped by @henrikdahl as a [pull request](https://github.com/zeit/hyper/pull/1560)
+
+## Licence
+
+[MIT](LICENSE.md)
---
+++ index.js
@@ -0,0 +1,85 @@
+const merge = require('lodash.merge');
+
+const defaultConfig = {
+  debug: false,
+  default: true
+};
+
+let config = defaultConfig;
+let isActive = false;
+let firstLoad = true;
+
+const debug = function () {
+  if (config.debug){
+    [].unshift.call(arguments, '|HYPER-ALWAYS-ON-TOP|');
+    console.log.apply(this, arguments);
+  }
+};
+
+let app_;
+
+function setActive(newState) {
+  if (isActive === newState) {
+    return;
+  }
+  debug('setActive', newState);
+  isActive = newState;
+  if (app_) {
+    debug('setActive on windows', newState);
+    app_.getWindows().forEach(win => win.setAlwaysOnTop(isActive));
+  }
+}
+
+exports.decorateConfig = globalConfig => {
+  if (globalConfig.alwaysOnTop) {
+    debug('load config', globalConfig.alwaysOnTop);
+    config = merge(JSON.parse(JSON.stringify(defaultConfig)), globalConfig.alwaysOnTop);
+    if (firstLoad) {
+      setActive(config.default);
+      firstLoad = false;
+    }
+  }
+
+  return globalConfig;
+};
+
+exports.onApp = (app) => {
+  debug('onApp', isActive);
+  app_ = app;
+  app_.getWindows().forEach(win => win.setAlwaysOnTop(isActive));
+};
+
+exports.onWindow = (win) => {
+  debug('onWindow', isActive);
+  win.setAlwaysOnTop(isActive);
+};
+
+exports.decorateMenu = (menu) => {
+  debug('decorateMenu isActive', isActive);
+  return menu.map(menuItem => {
+    if (menuItem.label !== 'View') {
+      return menuItem;
+    }
+
+    const newMenuItem = Object.assign({}, menuItem);
+    newMenuItem.submenu = [...newMenuItem.submenu];
+
+    newMenuItem.submenu.push({
+      type: 'separator'
+    });
+
+    newMenuItem.submenu.push({
+      label: 'Always On Top',
+      type: 'checkbox',
+      checked: isActive,
+
+      click: (item) => {
+        setActive(item.checked);
+      }
+    });
+
+    return newMenuItem;
+  });
+}
+
+
